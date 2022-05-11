3d7285b42eef179c82101f09a7efc0d71dbfdad6
Othon Felipe <ofbsantos@gmail.com>
2022-05-11 19:03:36 -0300

    Criação da rota POST da página de contato

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ app.use(express.static('public'));
 //CONFIG ROTAS
 app.get('/', (req, res) => {
     res.render('home', {
-        title: 'HomePage'
+        title: 'HomePage',
+        coberturas: [],
+        agenda: [
+                {id: 1, banda: 'Cairo Silva', local: 'Adega ZeroOnze', data: '10/01', horario: 21},
+                {id: 2, banda: 'Laura Moral', local: 'Boteco JL', data: '11/01', horario: 19},
+                {id: 3, banda: 'Mislane Silva', local: 'Âncora do rio', data: '12/01', horario: 13},
+        ]
     });
 });
 
@@ -44,6 +50,10 @@ app.get('*', (req, res) => {
         title: 'Página não encontrada',
         layout: 'nf'
     })
+})
+
+app.post('/send_contact', (req, res) => {
+    res.send('Contato enviado com sucesso!');
 })
 
 //CONFIG EXPRESS
