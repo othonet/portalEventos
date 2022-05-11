8ca5c235fbb5523eef3eeed135573477544da17b
Othon Felipe <ofbsantos@gmail.com>
2022-05-11 03:48:20 -0300

    Instalação e configuração do Handlbars - Criação dos diretórios das views - Criação e configuração das views

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 const porta = 3000;
 
-//ROTAS
+//CONFIG HANDLEBARS
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main', runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true
+}}));
+app.set('view engine', 'handlebars');
+
+//CONFIG ROTAS
 app.get('/', (req, res) => {
-    res.send('HomePage');
+    res.render('home', {
+        title: 'HomePage'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.send('Página não encontrada.');
 })
 
 //CONFIG EXPRESS
