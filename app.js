c64dacf71294563281fa6e0e2afd63014f816813
Othon Felipe <ofbsantos@gmail.com>
2022-05-13 04:51:53 -0300

    Instalação dos módulos: express-session e connect-flash - Configuração dos módulos express-session e connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+//MÓDULOS
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
+const session = require('express-session');
+const flash = require('connect-flash');
 const porta = 3000;
 
 //CONFIG´s
@@ -12,6 +15,20 @@ const porta = 3000;
             }
         }));
         app.set('view engine', 'handlebars');
+
+        //Sessões
+        app.use(session({
+            secret: 'qualquerCoisaSegura',
+            resave: true,
+            saveUninitialized: true
+        }));
+        app.use(flash());
+
+        //Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg');
+            res.locals.error_msg = req.flash('error_msg');
+        });
 
     //Arquivos estáticos
         app.use(express.static('public'));
